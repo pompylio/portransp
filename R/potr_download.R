1669c8811ec3137aa9d97924ad90d34feda6c604
pompylio <pompylio.lima@gmail.com>
2019-03-01 21:37:50 -0300

    Changed the help and format of the functions. - Excluded 'data' text in destfile exemple - Changed description and params in the help - New estructure for functions

--- R/potr_download.R
+++ R/potr_download.R
@@ -3,15 +3,15 @@
 #' This function can be used to download a dataset from the 'Portal da Transparencia'
 #' of Brazil - \url{http://www.portaltransparencia.gov.br}.
 #'
-#' @param opendata A character or numeric thats indicates a dataset selected for download.
-#' See x to meet valid input of the argument.
+#' @param opendata A character or numeric that indicates a dataset selected for download.
+#' See valid options in 'Details'.
 #' @param reference A character string or numeric input thats indicates a date reference to download.
-#' Valid date which may be 'yyyy' for year, 'mm' for month, #' 'dd' day or the join of these
-#' (yyyymm, yyyymmdd). Depends on the entrance requirements of the 'opendata'.
+#' Valid date which may be 'yyyy' for year, 'mm' for month, 'dd' day or the join of these
+#' (yyyymm, yyyymmdd). See valid options in 'Details'.
 #' @param destfile A character string with the name where the downloaded file is saved.
 #' If missing, the file will be saved to the current directory.
-#' @param filename File name to dataset. For all dataset defined by 'opendata' use the
-#' extension '.zip' to the end. If missing, the file name will be a title of opendata with a reference.
+#' @param filename File name to dataset. By default, the file name is the title of 'opendata'
+#' with the 'reference' and '.zip' extension.
 #' @param download.file.mode  The mode with which to write the file. If download.file.mode is not found
 #' and the operating system is windows, it will use the "wb".
 #' @param ... Others arguments of the \code{\link[utils:download.file]{download.file}} function.
@@ -20,22 +20,22 @@
 #' The function downloads files available in the repository of transparency portal -
 #' website of the federal government of Brazil on public transparency, with budgetary
 #' data and government management. The data disclosed in the Portal come from various
-#' sources of government information. See http://www.portaltransparencia.gov.br/ for
-#' more information.
+#' sources of government information. See http://www.portaltransparencia.gov.br/.
 #'
 #' The function arguments are specifications about the data set to be downloaded.
 #' 'opendata' is related to the content of the data, 'reference' to the date of
-#' availability of the file in the portal and 'destfile' to the location to be saved.
+#' availability of the file in the portal, 'destfile' to the location to be saved and
+#' 'filename' is the name of the file to be saved.
 #' Other '...' arguments are related to the download.file function of the 'utils' package.
 #'
 #' It is suggested that the values for '...' be reported only if the default setting
 #' does not work. See \code{\link[utils:download.file]{download.file}} for more
 #' information about these arguments and proxy settings.
 #'
-#' These are valid options for 'opendata' and 'reference' arguments:
+#' Valid options for 'opendata' and 'reference' arguments:
 #'
 #' \tabular{lllll}{
-#' id \tab description                                         \tab opendata                 \tab reference format\cr
+#' id \tab description                                         \tab opendata                 \tab reference       \cr
 #' 1  \tab Bolsa familia pagamentos                            \tab bolsa-familia-pagamentos \tab yyyymm          \cr
 #' 2  \tab Bolsa familia saques                                \tab bolsa-familia-saques     \tab yyyymm          \cr
 #' 3  \tab Erradicacao do trabalho infantil PETI               \tab peti                     \tab yyyymm          \cr
@@ -67,45 +67,48 @@
 #' }
 #'
 #' @examples
-#' potr_download(opendata = 19, reference = 2018)
-#' potr_download(opendata = 19, reference = 2018, destfile = "~/data")
-#' potr_download(opendata = 19, reference = 2018, destfile = "~/data", filename = "orcamento.zip")
-#' potr_download(opendata = "19", reference = "2018")
-#' potr_download(opendata = "19", reference = "2018", destfile = "~/data")
-#' potr_download(opendata = "19", reference = "2018", destfile = "~/data", filename = "orcamento.zip")
-#' potr_download(opendata = "orcamento-despesa", reference = "2018")
-#' potr_download(opendata = "orcamento-despesa", reference = 2018, destfile = "~/data")
-#' potr_download(opendata = "orcamento-despesa", reference = 2018, destfile = "~/data", filename = "orcamento.zip")
+#' potr_download(opendata = 19, reference = 2019)
+#' potr_download(opendata = 19, reference = 2019, destfile = "~/")
+#' potr_download(opendata = 19, reference = 2019, destfile = "~/", filename = "orcamento")
+#' potr_download(opendata = "19", reference = "2019")
+#' potr_download(opendata = "19", reference = "2019", destfile = "~/")
+#' potr_download(opendata = "19", reference = "2019", destfile = "~/", filename = "orcamento")
+#' potr_download(opendata = "orcamento-despesa", reference = "2019")
+#' potr_download(opendata = "orcamento-despesa", reference = 2019, destfile = "~/")
+#' potr_download(opendata = "orcamento-despesa", reference = 2019, destfile = "~/", filename = "orcamento")
 #' @export
 potr_download <- function(opendata, reference, destfile, filename, download.file.mode, ...) {
-  if (missing(opendata) || missing(reference))
-    stop("Valid input to 'opendata' and 'reference': ", potrms, call. = FALSE)
-  if (any(opendata == potrdt$dataset))
+  if (missing(opendata) || missing(reference)) {
+    stop("Valid input to 'opendata' and 'reference': ", potrms, call. = FALSE)}
+  if (any(opendata == potrdt$dataset)) {
     potrdt <- potrdt[which(potrdt$dataset == opendata), ]
-  else if (any(as.character(opendata) == potrdt$id))
+  } else if (any(as.character(opendata) == potrdt$id)) {
     potrdt <- potrdt[potrdt$id == as.numeric(opendata), ]
-  else stop("Valid input to 'opendata' and 'reference': ", potrms, call. = FALSE)
-  if (!nchar(reference) == nchar(potrdt$formatdate))
-    stop("Valid input to 'opendata' and 'reference': ", potrms, call. = FALSE)
-  if (missing(filename)) filename <- paste0(potrdt$dataset, "_", reference, ".zip")
-  else if (!grepl(pattern = ".zip", x = filename))
-    filename <- paste0(filename,".zip")
-  if (missing(destfile)) destfile <- filename
-  else
-    if (grepl("/", substr(destfile, nchar(destfile), nchar(destfile))))
-         destfile <- paste0(destfile, filename)
-    else destfile <- paste0(destfile, "/", filename)
-  potrurl <- paste0(
-    potrurl,
-    unlist(strsplit(potrdt$dataset, split = "_"))[1],
-    "/",
-    reference,
-    ifelse(potrdt$subitem == "", "", paste0("_", potrdt$subitem))
-    )
-  if (missing(download.file.mode) && Sys.info()["sysname"] == "Windows"){
-    download.file.mode <- "wb"
+  } else {
+    stop("Valid input to 'opendata' and 'reference': ", potrms, call. = FALSE)}
+  if (!nchar(reference) == nchar(potrdt$formatdate)) {
+    stop("Valid input to 'opendata' and 'reference': ", potrms, call. = FALSE)}
+  if (missing(filename)) {
+    filename <- paste0(potrdt$dataset, "_", reference, ".zip")
+  } else if (!grepl(pattern = ".zip*", x = filename)) {
+    filename <- paste0(filename, ".zip")}
+  if (missing(destfile)) {
+    destfile <- filename
+  } else {
+    if (grepl("/", substr(destfile, nchar(destfile), nchar(destfile)))) {
+      destfile <- paste0(destfile, filename)
+    } else {
+      destfile <- paste0(destfile, "/", filename)}
   }
+  potrurl <-
+    paste0(
+      potrurl,
+      unlist(strsplit(potrdt$dataset, split = "_"))[1],
+      "/",
+      reference,
+      ifelse(potrdt$subitem == "", "", paste0("_", potrdt$subitem)))
+  if (missing(download.file.mode) && Sys.info()["sysname"] == "Windows") {
+    download.file.mode <- "wb"}
   download.file(url = potrurl, destfile = destfile, mode = download.file.mode, ...)
   invisible(potrurl)
 }
-
