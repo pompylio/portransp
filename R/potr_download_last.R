6fc8137b79bbf7b847fcf7deb0c7fc689c91dc58
pompylio <pompylio.lima@gmail.com>
2019-04-18 15:45:45 -0300

    Fixes error in potr_download_last function and includes numref_ignore argument
    - Arguments of the while function were not working
    
    - Included numref_ignore argument to ignore the last or last records (reference date)

--- R/potr_download_last.R
+++ R/potr_download_last.R
@@ -9,6 +9,8 @@
 #' If missing, the file will be saved to the current directory.
 #' @param filename File name to dataset. By default, the file name is the title of 'opendata'
 #' with the 'reference' and '.zip' extension.
+#' @param numref_ignore Intenger or numeric. Number of references that will not be considered
+#' from the last file that will be downloaded.
 #' @param download.file.mode  The mode with which to write the file. If download.file.mode
 #' is not found and the operating system is windows, it will use the "wb".
 #' @param ... Others arguments of the \code{\link[utils:download.file]{download.file}} function.
@@ -72,44 +74,53 @@
 #' potr_download_last(opendata = "orcamento-despesa")
 #' potr_download_last(opendata = "orcamento-despesa", destfile = "~/")
 #' potr_download_last(opendata = "orcamento-despesa", destfile = "~/", filename = "orcamento")
+#' potr_download_last(opendata = "orcamento-despesa", destfile = "~/", numref_ignore = 1)
 #' @export
-potr_download_last <- function(opendata, destfile, filename, download.file.mode, ...) {
-  if (missing(opendata)) {
-    stop("Valid input to 'opendata' and 'reference': ", potrms, call. = FALSE)}
-  if (any(opendata == potrdt$dataset)) {
-    potrdt <- potrdt[which(potrdt$dataset == opendata), ]
-  } else if (any(as.character(opendata) == potrdt$id)) {
-    potrdt <- potrdt[potrdt$id == as.numeric(opendata), ]
-  } else {
-    stop("Valid input to 'opendata' and 'reference': ", potrms, call. = FALSE)}
-  opendata <- unlist(strsplit(potrdt$dataset, split = "_"))[1]
-  reference <- seq(from = as.Date("2013-01-01"), to = Sys.Date(), by = potrdt$by)
-  reference <- substr(gsub(pattern = "-", replacement = "", x = reference), 1, nchar(potrdt$formatdate))
-  subitem <- ifelse(potrdt$subitem == "", "", paste0("_", potrdt$subitem))
-  if (missing(filename)){
-    filename <- potrdt$dataset
-  }
-  if (grepl(pattern = ".zip*", x = filename)) {
-    filename <- substr(filename, 1, nchar(filename)-4)
-  }
-  if (missing(destfile)) {
-    destfile <- filename
-  } else {
-    if (grepl(pattern = "\\/*", x = destfile)){
-      destfile <- paste0(destfile, filename)
+potr_download_last <- function (opendata, destfile, filename, numref_ignore, download.file.mode, ...)
+{
+    if (missing(opendata))
+        stop("Valid input to 'opendata' and 'reference': ", potrms,
+            call. = FALSE)
+    if (any(opendata == potrdt$dataset))
+        potrdt <- potrdt[which(potrdt$dataset == opendata), ]
+    else if (any(as.character(opendata) == potrdt$id))
+        potrdt <- potrdt[potrdt$id == as.numeric(opendata), ]
+    else stop("Valid input to 'opendata' and 'reference': ",
+        potrms, call. = FALSE)
+    opendata <- unlist(strsplit(potrdt$dataset, split = "_"))[1]
+    reference <- seq(from = as.Date("2013-01-01"), to = Sys.Date(),
+        by = potrdt$by)
+    reference <- substr(gsub(pattern = "-", replacement = "",
+        x = reference), 1, nchar(potrdt$formatdate))
+    subitem <- ifelse(potrdt$subitem == "", "", paste0("_", potrdt$subitem))
+    if (missing(filename))
+        filename <- potrdt$dataset
+    if (missing(destfile))
+        destfile <- filename
+    else
+        destfile <- paste0(destfile, "/", filename)
+    if(missing(numref_ignore)){
+        i = length(reference)
     } else {
-      destfile <- paste0(destfile, "/", filename)
+        i = length(reference) - as.numeric(numref_ignore)
     }
+    file_dl <- paste0(destfile, "_", reference[length(i)], ".zip")
+    if (missing(download.file.mode) && Sys.info()["sysname"] ==
+        "Windows") {
+        download.file.mode <- "wb"
     }
-  i = length(reference)
-  file_dl <- paste0(destfile, "_", reference[i], ".zip")
-  if (missing(download.file.mode) && Sys.info()["sysname"] == "Windows"){
-    download.file.mode <- "wb"
-  }
-  while (file.exists(file_dl) == FALSE | i == 0) {
-    link <- paste0(potrurl, opendata, "/", reference[i], subitem)
-    file_dl <- paste0(destfile, "_", reference[i], ".zip")
-    try(download.file(url = link, destfile = file_dl, mode = download.file.mode, ...), silent = TRUE)
-    i <- i - 1
-  }
+    else {
+        download.file.mode <- "w"
+    }
+    while (file.exists(file_dl) == FALSE) {
+        link <- paste0(potrurl, opendata, "/", reference[i],
+            subitem)
+        file_dl <- paste0(destfile, "_", reference[i], ".zip")
+        try(download.file(url = link, destfile = file_dl,
+          mode = download.file.mode, ...), silent = FALSE)
+        i <- i - 1
+        if(i == 0) stop()
+    }
+    message(paste(file_dl, "saved"))
+    return(file_dl)
 }
